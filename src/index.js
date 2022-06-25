04bbc8df8a488b12374028021cf4857924a99506
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-25 19:16:00 +0100

    SyntaxError: Unexpected identifier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import inquirer from "inquirer";
+const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "testing",
-      name: "test",
-      choices: ["yamom", "bob", "node"],
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
-  });
+const questions = [
+  {
+    type: "input",
+    name: "test",
+    message: "testing",
+  },
+];
+
+const init = async () = {
+  const answers = await inquirer.prompt(questions)
+}
+
+init();
