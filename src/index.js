face5a93a2c2db39ab9914864380887f9b66c80f
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-26 20:28:54 +0100

    fixed func error and added folders

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ const inquirer = require("inquirer");
 
 const questions = [
   {
-    type: "input",
-    name: "test",
-    message: "testing",
+    type: "list",
+    name: "pickAnOption",
+    message: "Please select one of the following options to begin",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Quit",
+    ],
   },
 ];
 
-const init = async () = {
-  const answers = await inquirer.prompt(questions)
-}
+const init = async () => {
+  //let in progress = true
+
+  const answers = await inquirer.prompt(questions);
+};
 
 init();
