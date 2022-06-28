1fc4bdc6dc3ea51972269558fefac8ec17db7e96
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-28 11:00:06 +0100

    add pseudo code

--- src/index.js
+++ src/index.js
@@ -50,30 +50,70 @@ const init = async () => {
 
     if (pickAnOption === "viewAllDepartments") {
       console.log("Here are the departments...");
+      //display a table in the console with the id and name columns
+      //the id should be auto_inc? and the name will be: engineers, finance, legal, sales
     }
 
     if (pickAnOption === "viewAllRoles") {
       console.log("Here are all the roles...");
+      //display a table in the console with the columns id, title, department & salary
+      //title will be the roles, eg Sales Lead, Software Engineer, Lawyer etc
+      //each role will belong to a department eg SE = Engineering, Lawyer = Legal
     }
 
     if (pickAnOption === "viewAllEmployees") {
       console.log("Here are all the employees...");
+      //view a table in the console with the id, first_name, last-name, title, department, salary and the employee's manager
     }
 
     if (pickAnOption === "addADepartment") {
       console.log("DEPARTMENT ADDED!!");
+      //when i choose add a department
+      //i am prompted with the question 'what is the name of the department?'
+      //once i add a department
+      //it will update the departments in the tables
+      //i am presented with the pickAnOption list again
     }
 
     if (pickAnOption === "addARole") {
       console.log("ROLE ADDED!!");
+      //when i choose add a role
+      //i am prompted with the question 'what is the name of the role?'
+      //when i add a role
+      //i am prompted with the question 'what is the salary of the role?'
+      //when i add the salary
+      //i am prompted with the question 'what department does the role belong to?'
+      //i am given a list of the roles to choose from
+      //it will update the tables with role name and salary and department
+      //i am presented with the pickAnOption list again
     }
 
     if (pickAnOption === "addAnEmployee") {
       console.log("EMPLOYEE ADDED!!");
+      //when i choose add an employee
+      //i am prompted with the question 'what is the employee's first name?'
+      //i enter the fn
+      //i am prompted with the question 'what is the employee's last name?'
+      //i enter the ln
+      //i am prompted with the question 'what is the employee's role?'
+      //i am given a list of the roles to choose from
+      //i am prompted with the question 'who is the employee's manager?'
+      //i am given a list of the managers to choose from
+      //it will update the employee in the tables
+      //i am presented with the pickAnOption list again
     }
 
     if (pickAnOption === "updateEmployeeRole") {
       console.log("Employee role updated...");
+      //when i choose update employee role
+      //i am prompted with the question 'which employee do you want to update?'
+      //i am given a list of employees to choose from
+      //when i select the employee
+      //i am prompted with the question 'Which role do you want to assign to the employee?'
+      //i am given a list of roles to choose from
+      //once i pick a role
+      //the tables update
+      //i am presented with the pickAnOption list again
     }
 
     if (pickAnOption === "quit") {
